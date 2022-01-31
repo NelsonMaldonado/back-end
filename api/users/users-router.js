3aa79eaf15ea5da12403a7690a4d78adecf25e76
Nelson Maldonado <play2nino@gmail.com>
2022-01-31 13:17:18 -0800

    re organizing code

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,2 +1,19 @@
-const router = require('express').Router()
-const model = require('.')
+const router = require("express").Router()
+const model = require("./users-model")
+
+router.get("/", (req, res) => {
+  res.send("getting all users")
+})
+
+router.post("/register", (req, res) => {
+  res.status(201).send("user registered")
+})
+
+// server.get("/api/users", async (req, res) => {
+//     res.json(await getAllUsers())
+//   })
+
+//   server.post("/api/users", async (req, res) => {
+//     res.status(201).json(await insertUser(req.body))
+//   })
+module.exports = router
