68d1c76fe7fdcdcbbd5a337946ef10af3cc053f4
Nelson Maldonado <play2nino@gmail.com>
2022-01-31 13:18:16 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -11,16 +11,8 @@ server.use(cors())
 
 server.use("/api/users", usersRouter)
 
-server.get("/", (req, res) => {
-  res.send("The / endpoint is working")
-})
-
-server.get("/api/users", async (req, res) => {
-  res.json(await getAllUsers())
-})
-
-server.post("/api/users", async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+// server.get("/", (req, res) => {
+//   res.send("The / endpoint is working")
+// })
 
 module.exports = server
