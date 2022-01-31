affcfd42fc324a75d3bd72c81449850dd0e839a2
Nelson Maldonado <play2nino@gmail.com>
2022-01-31 10:06:59 -0800

    added .env file and installed dependencies

--- api/server.js
+++ api/server.js
@@ -17,6 +17,10 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+server.get('/',(req,res)=>{
+  res.send('The / endpoint is working')
+})
+
 server.get('/api/users', async (req, res) => {
   res.json(await getAllUsers())
 })
