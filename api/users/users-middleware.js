4117c7eb6b5478163f36f8a0c80e6798610312b1
Nelson Maldonado <play2nino@gmail.com>
2022-01-31 13:27:01 -0800

    working on middleware

---
+++ api/users/users-middleware.js
@@ -0,0 +1,5 @@
+const model = require("./users-router")
+
+const registerPayload = (req, res, next) => {
+  console.log("Checking username phone number and more")
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -4,14 +4,13 @@ const model = require("./users-model")
 router.get("/", (req, res) => {
   res.send("getting all users")
 })
-
-router.post("/register", (req, res) => {
-  res.status(201).send("user registered")
-})
-
-// server.get("/api/users", async (req, res) => {
+// router.get("/", async (req, res) => {
 //     res.json(await getAllUsers())
 //   })
+
+router.post("/register", (req, res) => {
+  res.send("user registered")
+})
 
 //   server.post("/api/users", async (req, res) => {
 //     res.status(201).json(await insertUser(req.body))
